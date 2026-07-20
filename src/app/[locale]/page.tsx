02a509a950e7a4602084263daf6e6f69a5b6741d
Rodrigue Nyassa <nyassarodrigue3@gmail.com>
2026-07-20 15:15:48 +0100

    refactoring

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,11 @@
+import { getTranslations } from "next-intl/server";
+
+export default async function HomePage() {
+  const t = await getTranslations("Login");
+
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">{t - "title"}</h1>
+    </main>
+  );
+}
